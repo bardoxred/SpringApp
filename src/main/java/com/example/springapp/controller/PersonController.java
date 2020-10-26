739b640364267b12492996565770a6c74d594ade
Konrad <wesolowskikonrad28@gmail.com>
2020-10-26 12:39:09 +0100

    adding database

--- src/main/java/com/example/springapp/controller/PersonController.java
+++ src/main/java/com/example/springapp/controller/PersonController.java
@@ -3,9 +3,12 @@ package com.example.springapp.controller;
 import com.example.springapp.model.Person;
 import com.example.springapp.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
 
 import java.util.List;
+
 import java.util.UUID;
 
 @RequestMapping("/person")
@@ -19,7 +22,7 @@ public class PersonController {
         this.personService = personService;
     }
     @PostMapping
-    public void addPerson(@RequestBody Person person){
+    public void addPerson(@Valid @NonNull @RequestBody Person person){
         personService.addPerson(person);
     }
     @GetMapping
@@ -35,7 +38,7 @@ public class PersonController {
         personService.deletePerson(id);
     }
     @PutMapping(path = "{id}")
-    public void updatePersonById(@PathVariable("id") UUID id, @RequestBody Person person){
+    public void updatePersonById(@PathVariable("id") UUID id, @Valid @NonNull @RequestBody Person person){
         personService.updatePerson(id,person);
     }
 }
